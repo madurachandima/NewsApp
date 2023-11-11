2d9197c2573575a2fb5edc7d34b486066abff000
MaduraDiasTreinetic <40019782+madurachandima@users.noreply.github.com>
2023-11-11 10:20:44 +0530

    Initial commit

---
+++ app/src/main/java/com/loc/newsapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.loc.newsapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import com.loc.newsapp.domain.usecases.AppEntryUseCases
+import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
+import com.loc.newsapp.ui.theme.NewsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var appEntryUseCases: AppEntryUseCases
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        lifecycleScope.launch {
+            appEntryUseCases.readAppEntry().collect {
+                Log.d("Test", it.toString())
+            }
+        }
+        setContent {
+            NewsAppTheme {
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
+                }
+            }
+        }
+    }
+}
